120304bc87b4d2a8c2f8c60a6e121070de9864f7
adirahman <35604216+kodingku@users.noreply.github.com>
2018-07-11 11:56:31 +0700

    Update formPemohon.php

---
+++ application/views/front/pip/formPemohon.php
@@ -0,0 +1,96 @@
+<div class="container">
+
+		    	<h1 class="text-center">Form Permohonan Layanan Informasi Publik</h1>
+				<h1 class="text-center">PPPPTK PENJAS dan BK</h1>
+		    	<form class="g-brd-around g-brd-gray-light-v4 g-pa-30 g-mb-30">
+		    		<fieldset class="form-group g-mb-25">
+<!--					    <legend class="g-font-size-default">Apakah anda bersedia mengungkapkan identitas anda ?</legend>
+					    <div class="form-check">
+					      <label class="form-check-label">
+					        <input type="radio" class="form-check-input mr-1" name="optionsRadios" id="optionsRadios1" value="option1" checked="">Ya
+					      </label>
+					    </div>
+					    <div class="form-check">
+					      <label class="form-check-label">
+					        <input type="radio" class="form-check-input mr-1" name="optionsRadios" id="optionsRadios2" value="option2">Tidak
+					      </label>
+					    </div> -->
+					    
+					    <div class="form-group g-mb-25">
+						    <label>Nama Lengkap</label>
+						    <input type="text" class="form-control rounded-0 form-control-md"  placeholder="">
+						 </div>
+						 <div class="form-group g-mb-25">
+						    <label>Alamat Email</label>
+						    <input type="text" class="form-control rounded-0 form-control-md"  placeholder="">
+						 </div>
+						 <div class="form-group g-mb-25">
+						    <label>Nomor Telepon atau Handphone</label>
+						    <input type="text" class="form-control rounded-0 form-control-md"  placeholder="">
+						 </div>
+						 <div class="form-group g-mb-25">
+						    <label>Nomor Identitas</label>
+						    <input type="text" class="form-control rounded-0 form-control-md"  placeholder="KTP atau SIM">
+						 </div>
+						 <div class="form-group g-mb-25">
+						 	<label for="exampleSelect1">Pilih informasi yang dibutuhkan</label>
+						    <select class="form-control rounded-0" id="exampleSelect1">
+						      <option></option>
+						      <option>Peminjaman Sarana dan Prasarana</option>
+						      <option>Kerjasama</option>
+						      <option>Pendidikan dan Pelatihan</option>
+						      <option>Lainnya</option>
+						    </select>
+						  </div>
+						  <div class="form-group g-mb-25">
+						    <label>Tuliskan jenis informasi yang dibutuhkan</label>
+						    <textarea class="form-control rounded-0 form-control-md" id="exampleTextarea" rows="6"></textarea>
+													 </div>
+
+						 <div class="form-group g-mb-25">
+						    <label>Kegunaan atau kebermanfaatan informasi</label>
+						    <textarea class="form-control rounded-0 form-control-md" placeholder="" id="exampleTextarea" rows="6"></textarea>
+						 </div>
+
+<!--						 <div class="form-group g-mb-25">
+						    <label>Tempat Kejadian</label>
+						    <input type="text" class="form-control rounded-0 form-control-md"  placeholder="Tempat Kejadian">
+						 </div>
+
+						 <div class="form-group g-mb-25">
+						    <label>Tanggal Kejadian</label>
+						    <input type="date" class="form-control rounded-0 form-control-md"  placeholder="Tanggal Kejadian">
+						 </div>
+
+						 <div class="form-group g-mb-25">
+						    <label for="exampleTextarea">Kronologis</label>
+						    <textarea class="form-control rounded-0 form-control-md" id="exampleTextarea" rows="6"></textarea>
+						  </div>
+
+						  <div class="form-group g-mb-25">
+						    <label for="exampleTextarea">Keterangan Tambahan</label>
+						    <textarea class="form-control rounded-0 form-control-md" id="exampleTextarea" rows="6"></textarea>
+						  </div> -->
+
+<!--						  <div class="form-group g-mb-25">
+						    <label for="exampleInputFile">Unggah hasil pemindaian atau photo kartu identitas</label>
+						    <input type="file" class="form-control-file" id="exampleInputFile" aria-describedby="fileHelp">
+						    <small id="fileHelp" class="form-text text-muted">maksimum file 20 mb</small>
+						  </div> -->
+						  <div class="form-check">
+						  	<label>Klik bila anda setuju untuk menyatakan bahwa data yang anda masukan ke dalam form ini adalah benar</label><br>
+						    <label class="form-check-label g-mb-20">
+						      <input type="checkbox" class="form-check-input mr-1" id="chkSend">Setuju
+						    </label>
+						  </div> 
+						  <div class="form-group">
+<!--						  	<label class="control-label">
+						  		Dengan ini Saya melaporkan segala bentuk dan atau tindakan yang terjadi yang dapat merugikan orang lain/ kelompok/ instansi tertentu dalam kondisi sadar dan tanpa tekanan dari pihak manapun. Apabila dikemudian hari Saya terbukti melakukan laporan palsu / bohong / hoax maka Saya bersedia dituntut secara hukum sesuai dengan hukum yang berlaku di Negara Kesatuan Republik Indonesia
+						  	</label> -->
+						  </div>
+						  <div class="form-group" id="myDIV" style="display: none">
+						  	<a href="#!" class="btn btn-md u-btn-3d u-btn-blue g-mr-10 g-mb-15">Kirim</a>
+						  </div>
+					  </fieldset>
+		    	</form>
+		    </div>
